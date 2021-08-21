2cdbd4cb2e4daf3692d5d2c0fe91a5440fa2fb77
Sammy Mohamed <scooby1223sammy@gmail.com>
2021-08-21 11:04:51 -0400

    Migrated UI Lib back to normal src root structure

---
+++ projects/fuego-ui/src/public-api.ts
@@ -0,0 +1,37 @@
+/*
+ * Public API Surface of fuego-ui
+ */
+
+export * from "./lib/autocomplete/autocomplete.component";
+export * from "./lib/autocomplete/autocomplete.module";
+
+export * from "./lib/card/card.component";
+export * from "./lib/card/card.module";
+
+export * from "./lib/form-fields/field/field.component";
+export * from "./lib/form-fields/field/field-error.directive";
+export * from "./lib/form-fields/field/field-hint.directive";
+export * from "./lib/form-fields/field/field-input.directive";
+export * from "./lib/form-fields/field/field-label.directive";
+export * from "./lib/form-fields/field/field-placeholder.directive";
+export * from "./lib/form-fields/field/field-prefix.directive";
+export * from "./lib/form-fields/field/field-suffix.directive";
+export * from "./lib/form-fields/field/field.module";
+
+export * from "./lib/form-fields/select/select.component";
+export * from "./lib/form-fields/select/select.module";
+
+export * from "./lib/hr/hr.component";
+export * from "./lib/hr/hr.module";
+
+export * from "./lib/image/image.component";
+export * from "./lib/image/image.module";
+
+export * from "./lib/search-box/search-box.component";
+export * from "./lib/search-box/search-box.module";
+
+export * from "./lib/tag/tag.component";
+export * from "./lib/tag/tag.module";
+
+export * from "./lib/text-image/text-image.component";
+export * from "./lib/text-image/text-image.module";
